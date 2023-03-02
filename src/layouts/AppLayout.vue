8763914146ad676d7cc01923258a3de66540a5d3
jamsimson <mwattanachant@gmail.com>
2023-03-02 16:15:21 +0700

    push

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="layout-background">
+    <!-- heading bar -->
+    <q-header elevated class="bg-white text-black">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="D:\minispace\Alpha\alpha-app\public\img\alpha.png">
+          </q-avatar>
+          Alpha Bank
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+     <q-footer reveal class="bg-white rounded-borders fixed-bottom">
+      <q-tabs v-model="tab" class="q-mt-sm text-blue-grey-10" active-color="#E5E5E5" no-caps narrow-indicator dense align="justify">
+        <q-route-tab to="#" class="t" name="home" icon="fas fa-home-user" label="Home" />
+        <q-route-tab to="#" class="" name="directory" icon="fas fa-list-ul" label="Transfer" />
+        <q-route-tab to="#" class="" name="directory" icon="fas fa-list-ul" label="Scan" />
+        <q-route-tab to="#" class="" name="search" icon="fas fa-search" label="Services" />
+        <q-route-tab to="#" class="" name="about" icon="fas fa-info-circle" label="Setting" />
+      </q-tabs>
+    </q-footer>
+
+  </q-layout>
+</template>
