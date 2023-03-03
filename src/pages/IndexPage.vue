04cfd064587e5daa0921add39f202649d3bbea4c
kittipan2206 <s6330613017@phuket.psu.ac.th>
2023-03-03 19:53:03 +0700

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,42 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="text-center">
+      <div>
+        <img
+          alt="Quasar logo"
+          src="~assets/quasar-logo-vertical.svg"
+          style="width: 200px; height: 200px"
+        />
+      </div>
+      <div class="text-h3">
+        <p>ALPHA</p>
+      </div>
+      <q-separator />
+      <div class="text-h5">
+        <p>Let's Get Started</p>
+      </div>
+      <div class="text-center">
+        <p>Your finance, your control, with Alpha Banking.</p>
+      </div>
+      <q-btn
+        class="q-mt-xl full-width"
+        color="green"
+        text-color="purple"
+        unelevated
+        to="/"
+        label="Sign up"
+        no-caps
+        width="200px"
+      />
+      <p>Already have an account? <a href=""></a></p>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
