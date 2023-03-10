bf774250e08fe434bd14ba6a4e3cf947364c784e
jamsimson <mwattanachant@gmail.com>
2023-03-10 14:45:43 +0700

    update camera

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -50,12 +50,26 @@ export default {
       this.showCamera = false;
     },
     async onInit(promise) {
+      // show loading indicator
+
       try {
-        await promise;
-      } catch (e) {
-        console.error(e);
-      } finally {
-        this.showScanConfirmation = this.camera === "off";
+        const { capabilities } = await promise;
+
+        // successfully initialized
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          // user denied camera access permisson
+        } else if (error.name === "NotFoundError") {
+          // no suitable camera device installed
+        } else if (error.name === "NotSupportedError") {
+          // page is not served over HTTPS (or localhost)
+        } else if (error.name === "NotReadableError") {
+          // maybe camera is already in use
+        } else if (error.name === "OverconstrainedError") {
+          // did you requested the front camera although there is none?
+        } else if (error.name === "StreamApiNotSupportedError") {
+          // browser seems to be lacking features
+        }
       }
     },
   },
